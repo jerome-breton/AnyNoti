7de6b611dcd5d3d67d07e53e6c038ced4b72d816
unknown <Jerome@MaD-Fixe.(none)>
2011-12-12 07:58:47 +0100

    New functionality : Embedded in extension read items RSS Service New icons set with credits in Readme Toaster redesign

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,6 @@
 if(typeof accounts !== 'undefined'){
-    accounts[0] = {
+	var a = 0;
+    accounts[a++] = {
         name:'Jerome Breton (gmail)',
         type:'gmail',
         serviceOptions : {
@@ -8,7 +9,7 @@ if(typeof accounts !== 'undefined'){
         frequency:'15'
     };
 
-    accounts[1] = {
+    accounts[a++] = {
         name:'Sqli (gmail)',
         type:'gmail',
         serviceOptions : {
@@ -21,7 +22,7 @@ if(typeof accounts !== 'undefined'){
         frequency:'15'
     };
 
-    accounts[2] =  {
+    accounts[a++] =  {
         name:'Newsletter (gmail)',
         type:'gmail',
         serviceOptions : {
@@ -33,6 +34,40 @@ if(typeof accounts !== 'undefined'){
 		},
         frequency:'15'
     };
+
+	accounts[a++] = {
+		name:'jQuery',
+		type:'rss',
+		serviceOptions : {
+			feedUrl : "http://feeds.feedburner.com/jquery/",
+			itemHidingBehaviour:1
+		}
+	};
+
+	accounts[a++] = {
+		name:'Thinkgeek',
+		type:'rss',
+		serviceOptions : {
+			feedUrl : "http://feeds.feedburner.com/thinkgeek/whatsnew",
+			itemHidingBehaviour:1
+		},
+		serviceOverloads : {
+			color:[133,67,30,255]
+		}
+	};
+
+	accounts[a++] = {
+		name:'Frandroid',
+		type:'rss',
+		serviceOptions : {
+			feedUrl : "http://www.frandroid.com/feed/atom/",
+			itemHidingBehaviour:2
+		},
+		serviceOverloads : {
+			color:[110,166,39,255]
+		}
+	};
+
 
 }
 
@@ -58,6 +93,85 @@ var utils = {
         return this._backgroundPage;
     },
 
+	//Global parameters
+	options:{
+		maxSummaryLength : 100,
+		toasterTimeout : 15000
+	},
+
+	//Constants
+	constants:{
+		itemHidingBehaviour:{
+			none:false,
+			onClick:1,
+			onDisplay:2
+		}
+	},
+
+	items:{
+		read:null,
+		markAsRead:function(index,id){
+			var readItems = utils.items.getRead(index);
+			readItems[id] = new Date();
+			utils.items.save(index,readItems);
+			var bgPage = utils.getBackgroundPage();
+			if(bgPage.results[index] && bgPage.results[index].count){
+				bgPage.results[index].count--;
+				if(bgPage.results[index].list){
+					delete bgPage.results[index].list[id];
+				}
+			}
+			bgPage.background.refreshBadge();
+			return true;
+		},
+		markAsUnread:function(index,id){
+			var readItems = utils.items.getRead(index);
+			delete readItems[id];
+			utils.items.save(index,readItems);
+			return true;
+		},
+		isRead:function(index,id){
+			var readItems = utils.items.getRead(index);
+			var isRead = readItems[id] || false;
+			if(isRead){	//Touch the entry to set its date to now
+				readItems[id] = new Date();
+				utils.items.save(index,readItems);
+			}
+			return isRead;
+		},
+		getRead:function(index){
+			var bgPage = utils.getBackgroundPage();
+			if(!bgPage.utils.items.read){
+				try{
+					bgPage.utils.items.read = JSON.parse(localStorage["readItems"]);
+				}catch(e){
+					bgPage.utils.items.read = {};
+				}
+
+				//Clean older entries
+				var now = new Date();
+				jQuery.each(bgPage.utils.items.read,function(account,readItems){
+					jQuery.each(readItems,function(id,date){
+						date = new Date(date);	//Date is stored as a string
+						bgPage.utils.items.read[account][id] = date;
+						if((now-date) > 30*24*60*60*1000){
+							delete bgPage.utils.items.read[account][id];
+						}
+					});
+				});
+			}
+			if(!bgPage.utils.items.read[index]){
+				bgPage.utils.items.read[index] = {};
+			}
+			return bgPage.utils.items.read[index];
+		},
+		save:function(index,readItems){
+			var bgPage = utils.getBackgroundPage();
+			bgPage.utils.items.read[index] = readItems;
+			localStorage["readItems"] = JSON.stringify(bgPage.utils.items.read);
+		}
+	},
+
 	//Set of functions for manipulating colors
     color:{
         notToDark:function(color){   this._log('notToDark');
@@ -77,23 +191,35 @@ var utils = {
             ];
         },
         getLighter:function(color){   this._log('getLighter');
-            return this.notToLight([
-                color[0]+64,
-                color[1]+64,
-                color[2]+64,
+			color = this.rectify(color);
+            return this.rectify([
+                (color[0]+32)*1.25,
+                (color[1]+32)*1.25,
+                (color[2]+32)*1.25,
                 color[3]
             ]);
         },
         getDarker:function(color){   this._log('getDarker');
-            return this.notToDark([
-                color[0]-64,
-                color[1]-64,
-                color[2]-64,
+			color = this.rectify(color);
+            return this.rectify([
+                (color[0]-32)*.75,
+                (color[1]-32)*.75,
+                (color[2]-32)*.75,
                 color[3]
             ]);
         },
+		rectify:function(color){	this._log('rectify');
+			color = this.notToLight(this.notToDark(color));
+			return [
+				Math.round(color[0]),
+				Math.round(color[1]),
+				Math.round(color[2]),
+				Math.round(color[3])
+			];
+		},
 		toCssRgba:function(color){   this._log('toCssRgba');
-		  return 'rgba('+color[0]+','+color[1]+','+color[2]+','+Math.round(color[3]/255,2)+')'
+			color = this.rectify(color);
+			return 'rgba('+color[0]+','+color[1]+','+color[2]+','+Math.round(color[3]/255,2)+')'
 		},
         _log:function(msg){
             utils._log('[color]'+msg);
@@ -101,7 +227,12 @@ var utils = {
     },
 	loadServices:function(){   this._log('loadServices');
 		utils._include("../services/noop.js");
+		utils._include("../services/rss.js");
 		utils._include("../services/gmail.js");
+	},
+	//Strip HTML Tags (but keeps &chars;)
+	stripTags:function(str){	this._log('stripTags');
+		return str.replace(/(<([^>]+)>)/ig,"");
 	},
 	_include:function(file){   this._log('_include '+file);
 		var script  = document.createElement('script');
@@ -126,7 +257,7 @@ var utils = {
 		}
 		return params;
 	},
-    _log:function(msg){ //return;
+    _log:function(msg){ return;
         console.log('[utils]'+msg);
     }
 };
